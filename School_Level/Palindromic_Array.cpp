b670495a62a524d6094908a506682a891edfa74f
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2022-12-15 12:44:59 +0530

    Create Palindromic_Array.cpp

---
+++ School_Level/Palindromic_Array.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*Complete the function below*/
+
+class Solution {
+public:
+    int PalinArray(int a[], int n)
+    {
+    	// code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i = 0; i < n; i++)
+			cin>>a[i];
+		Solution obj;
+		cout<<obj.PalinArray(a,n)<<endl;
+	}
+}
+// } Driver Code Ends
